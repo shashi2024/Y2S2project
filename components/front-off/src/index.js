e21a19e97877a10869c8a07dd0c362b09e8cfe68
Sashi2000 <sashinisithara20@gmail.com>
2024-04-04 13:51:29 +0530

    updates

--- components/front-off/src/index.js
+++ components/front-off/src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Top from './Top.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Top/>
   </React.StrictMode>
 );
 
