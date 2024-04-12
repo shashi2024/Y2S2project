5c829c092b81aca9686a6e6f0d079fd305c8562c
Sashi2000 <sashinisithara20@gmail.com>
2024-04-12 17:34:07 +0530

    schema creation

---
+++ backend/src/api/model/userRoll.model.js
@@ -0,0 +1,18 @@
+// Import the mongoose module
+import mongoose from "mongoose";
+
+// Define the schema
+const UserRollSchema = new mongoose.Schema({
+  uID: {
+    type: Number,
+    required: true,
+  },
+  rID: {
+    type: Number,
+    required: true,
+  },
+});
+
+// Create the model from the schema and export it
+const UserRoll = mongoose.model("userRoll", UserRollSchema);
+export default UserRoll;
