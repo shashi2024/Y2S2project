d78a84f6d8cd60178073027484da43044ba0d677
Manilka <ruvanipradeepa@gmail.com>
2024-04-20 20:55:21 +0530

    Maintenance Components

---
+++ frontend/backoffice/src/pages/CreateTask.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import Sidebar from "../partials/Sidebar";
+import Header from "../partials/Header";
+import Button from "../components/Button";
+
+const RestaurantDashboard = () => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  return (
+    <div className="flex h-screen overflow-hidden">
+      <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+
+      <div className="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
+        {/*  Site header */}
+        <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
+        <main>
+          <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
+            <h1 className="text-slate-800 font-bold text-3xl">Dashboard</h1>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default RestaurantDashboard;
