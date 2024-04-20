8f1a2d0cfeba9cfb4371ad7867e78d8ccc5ed867
Manilka <ruvanipradeepa@gmail.com>
2024-04-20 21:59:49 +0530

    create task form

--- frontend/backoffice/src/pages/CreateTask.jsx
+++ frontend/backoffice/src/pages/CreateTask.jsx
@@ -1,9 +1,9 @@
 import { useState } from "react";
 import Sidebar from "../partials/Sidebar";
 import Header from "../partials/Header";
-import Button from "../components/Button";
+import CreateTaskForm from "../partials/createTask/CreateTaskForm";
 
-const RestaurantDashboard = () => {
+const CreateTask = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
@@ -14,13 +14,16 @@ const RestaurantDashboard = () => {
         {/*  Site header */}
         <Header sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} />
         <main>
-          <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
-            <h1 className="text-slate-800 font-bold text-3xl">Dashboard</h1>
+          <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto bg-background ">
+          <div className="bg-white p-6 rounded-lg shadow-lg">
+              <CreateTaskForm />
+            </div>
           </div>
+          
         </main>
       </div>
     </div>
   );
 };
 
-export default RestaurantDashboard;
+export default CreateTask;
---
+++ frontend/backoffice/src/partials/createTask/CreateTaskForm.jsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+import Button from '../../components/Button';
+
+function CreateTaskForm() {
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+      <h1 className="text-2xl font-bold">Create Task</h1>
+      <hr className="border-t border-second_background mt-2 mb-12"/>
+
+      <div>
+        <label className="block text-sm font-medium">Task title</label>
+        <input {...register("title")} placeholder="Enter Title" className="mt-1 block w-full rounded-md border-second_background shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
+      </div>
+
+      <div className="flex space-x-4">
+        <div className="w-1/2">
+          <label className="block text-sm font-medium">Task Type</label>
+          <select {...register("type")} className="mt-1 block w-full rounded-md border-second_background shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+            <option value="option1">Option 01</option>
+            <option value="option2">Option 02</option>
+            <option value="option3">Option 03</option>
+          </select>
+        </div>
+
+        <div className="w-1/2">
+          <label className="block text-sm font-medium">Task Duration</label>
+          <input {...register("duration")} placeholder="Duration" className="mt-1 block w-full rounded-md border-second_background shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
+        </div>
+      </div>
+
+      <div className="flex space-x-4">
+        <div className="w-1/3">
+          <label className="block text-sm font-medium">Date</label>
+          <input {...register("date")} type="date" className="mt-1 block w-full rounded-md border-second_background shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
+        </div>
+
+        <div className="w-1/3">
+          <label className="block text-sm font-medium">Start Time</label>
+          <input {...register("startTime")} type="time" className="mt-1 block w-full rounded-md border-second_background shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
+        </div>
+
+        <div className="w-1/3">
+          <label className="block text-sm font-medium">End Time</label>
+          <input {...register("endTime")} type="time" className="mt-1 block w-full rounded-md border-second_background shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
+        </div>
+      </div>
+
+      <div>
+        <label className="block text-sm font-medium">Task Description</label>
+        <textarea {...register("description")} className="mt-1 block w-full rounded-md border-second_background shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
+      </div>
+
+      <div className="flex space-x-4">
+        <div className="w-1/2">
+          <label className="block text-sm font-medium">Asset</label>
+          <select {...register("asset")} className="mt-1 block w-full rounded-md border-second_background shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+            <option value="option1">Option 01</option>
+            <option value="option2">Option 02</option>
+            <option value="option3">Option 03</option>
+          </select>
+        </div>
+
+        <div className="w-1/2">
+          <label className="block text-sm font-medium">Location</label>
+          <input {...register("location")} placeholder="Location" className="mt-1 block w-full rounded-md border-second_background shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
+        </div>
+      </div>
+
+      <div>
+        <label className="block text-sm font-medium">Assignee</label>
+        <select {...register("assignee")} className="mt-1 block w-full rounded-md border-second_background shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+          <option value="option1">Option 01</option>
+          <option value="option2">Option 02</option>
+          <option value="option3">Option 03</option>
+        </select>
+      </div>
+
+      <Button>Create Task</Button>
+    </form>
+  );
+}
+
+export default CreateTaskForm;
