034f6c8eda68bbbe2c821b33b0f6eabbe4164f85
Manilka Navod <manilka.projects@gmail.com>
2024-04-21 01:41:58 +0530

    backend connect

--- frontend/backoffice/src/pages/CreateTask.jsx
+++ frontend/backoffice/src/pages/CreateTask.jsx
@@ -2,9 +2,11 @@ import { useState } from "react";
 import Sidebar from "../partials/Sidebar";
 import Header from "../partials/Header";
 import CreateTaskForm from "../partials/createTask/CreateTaskForm";
+import { useNavigate } from "react-router-dom";
 
 const CreateTask = () => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
+  const navigate = useNavigate();
 
   return (
     <div className="flex h-screen overflow-hidden">
@@ -16,7 +18,7 @@ const CreateTask = () => {
         <main>
           <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto bg-background ">
             <div className="bg-white p-6 rounded-lg shadow-lg">
-              <CreateTaskForm />
+              <CreateTaskForm history={navigate} />
             </div>
           </div>
         </main>
--- frontend/backoffice/src/partials/createTask/CreateTaskForm.jsx
+++ frontend/backoffice/src/partials/createTask/CreateTaskForm.jsx
@@ -1,66 +1,132 @@
-import React from 'react';
-import { useForm } from 'react-hook-form';
-import Button from '../../components/Button';
+import React from "react";
+import { useForm } from "react-hook-form";
+import { useNavigate } from "react-router-dom";
+import Button from "../../components/Button";
+import axios from "axios";
 
 function CreateTaskForm() {
   const { register, handleSubmit } = useForm();
+  const navigate = useNavigate();
 
-  const onSubmit = (data) => {
-    console.log(data);
+  const onSubmit = async (data) => {
+    // check if taskType is 'HouseKeeping'
+
+    const date = new Date(data.date);
+    const startTime = new Date(
+      date.setHours(data.startTime.split(":")[0], data.startTime.split(":")[1])
+    );
+    const endTime = new Date(
+      date.setHours(data.endTime.split(":")[0], data.endTime.split(":")[1])
+    );
+
+    if (startTime > endTime) {
+      alert("Start time must be less than end time");
+      return;
+    }
+
+    if (data.taskType === "Housekeeping ") {
+      data.roomId = data.location;
+    }
+
+    const taskData = {
+      ...data,
+      startTime,
+      endTime,
+    };
+
+    try {
+      console.log(JSON.stringify(taskData));
+
+      const response = await axios.post("http://localhost:5000/task", taskData);
+      console.log(response.data);
+      navigate("/maintenance/manage-tasks");
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
       <h1 className="text-2xl font-bold">Create Task</h1>
-      <hr className="border-t border-second_background mt-2 mb-12"/>
+      <hr className="border-t border-second_background mt-2 mb-12" />
 
       <div>
         <label className="block text-sm font-medium">Task title</label>
-        <input {...register("title")} placeholder="Enter Title" className="mt-1 block w-full rounded-md border-second_background shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
+        <input
+          {...register("title")}
+          placeholder="Enter Title"
+          className="mt-1 block w-full rounded-md border-second_background shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+        />
       </div>
 
       <div className="flex space-x-4">
         <div className="w-1/2">
           <label className="block text-sm font-medium">Task Type</label>
-          <select {...register("type")} className="mt-1 block w-full rounded-md border-second_background shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
-            <option value="option1">Option 01</option>
-            <option value="option2">Option 02</option>
-            <option value="option3">Option 03</option>
+          <select
+            {...register("taskType")}
+            className="mt-1 block w-full rounded-md border-second_background shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+          >
+            <option value="Routine">Routine</option>
+            <option value="Housekeeping">Housekeeping</option>
+            <option value="Emergency">Emegerncy</option>
           </select>
         </div>
 
-        <div className="w-1/2">
+        {/* <div className="w-1/2">
           <label className="block text-sm font-medium">Task Duration</label>
-          <input {...register("duration")} placeholder="Duration" className="mt-1 block w-full rounded-md border-second_background shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
-        </div>
+          <input
+            {...register("duration")}
+            placeholder="Duration"
+            className="mt-1 block w-full rounded-md border-second_background shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+          />
+        </div> */}
       </div>
 
       <div className="flex space-x-4">
         <div className="w-1/3">
           <label className="block text-sm font-medium">Date</label>
-          <input {...register("date")} type="date" className="mt-1 block w-full rounded-md border-second_background shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
+          <input
+            {...register("date")}
+            type="date"
+            className="mt-1 block w-full rounded-md border-second_background shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+          />
         </div>
 
         <div className="w-1/3">
           <label className="block text-sm font-medium">Start Time</label>
-          <input {...register("startTime")} type="time" className="mt-1 block w-full rounded-md border-second_background shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
+          <input
+            {...register("startTime")}
+            type="time"
+            className="mt-1 block w-full rounded-md border-second_background shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+          />
         </div>
 
         <div className="w-1/3">
           <label className="block text-sm font-medium">End Time</label>
-          <input {...register("endTime")} type="time" className="mt-1 block w-full rounded-md border-second_background shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
+          <input
+            {...register("endTime")}
+            type="time"
+            className="mt-1 block w-full rounded-md border-second_background shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+            required
+          />
         </div>
       </div>
 
       <div>
         <label className="block text-sm font-medium">Task Description</label>
-        <textarea {...register("description")} className="mt-1 block w-full rounded-md border-second_background shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
+        <textarea
+          {...register("description")}
+          className="mt-1 block w-full rounded-md border-second_background shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+        />
       </div>
 
       <div className="flex space-x-4">
         <div className="w-1/2">
           <label className="block text-sm font-medium">Asset</label>
-          <select {...register("asset")} className="mt-1 block w-full rounded-md border-second_background shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+          <select
+            {...register("assetId")}
+            className="mt-1 block w-full rounded-md border-second_background shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+          >
             <option value="option1">Option 01</option>
             <option value="option2">Option 02</option>
             <option value="option3">Option 03</option>
@@ -69,13 +135,25 @@ function CreateTaskForm() {
 
         <div className="w-1/2">
           <label className="block text-sm font-medium">Location</label>
-          <input {...register("location")} placeholder="Location" className="mt-1 block w-full rounded-md border-second_background shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50" />
+          <select
+            {...register("roomId")}
+            className="mt-1 block w-full rounded-md border-second_background shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+          >
+            <option value="">Select a location</option>
+            <option value="661bacca9703fb156fd972db">Location 1</option>
+            <option value="661bacca9703fb156fd972db">Location 2</option>
+            <option value="661bacca9703fb156fd972db">Location 3</option>
+            {/* Add more options as needed */}
+          </select>
         </div>
       </div>
 
       <div>
         <label className="block text-sm font-medium">Assignee</label>
-        <select {...register("assignee")} className="mt-1 block w-full rounded-md border-second_background shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50">
+        <select
+          {...register("assignee")}
+          className="mt-1 block w-full rounded-md border-second_background shadow-sm focus:border-indigo-300 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+        >
           <option value="option1">Option 01</option>
           <option value="option2">Option 02</option>
           <option value="option3">Option 03</option>
