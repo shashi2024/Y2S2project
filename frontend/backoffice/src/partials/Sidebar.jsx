9de1ff29d78ce59160b0a1c0c1be217aba002d1a
Manilka <ruvanipradeepa@gmail.com>
2024-04-20 20:24:55 +0530

    r

--- frontend/backoffice/src/partials/Sidebar.jsx
+++ frontend/backoffice/src/partials/Sidebar.jsx
@@ -398,7 +398,7 @@ function Sidebar({ sidebarOpen, setSidebarOpen }) {
                               }
                             >
                               <span className="text-sm font-medium lg:opacity-0 lg:sidebar-expanded:opacity-100 2xl:opacity-100 duration-200">
-                                Create Item
+                                Manage Food Item
                               </span>
                             </NavLink>
                           </li>
